f9303ef2e1cdd23b9ef49d24f12b25001b349d36
shanderb <s.hander8910@hotmail.com>
2025-08-27 23:01:58 -0300

    feat/migracao-de-static-para-angular - Adicionado propriedade para o valor.

--- src/app/models/despesa-simplificada.model.ts
+++ src/app/models/despesa-simplificada.model.ts
@@ -2,5 +2,6 @@ import { CommomInterface } from "./commom.model";
 
 export interface DespesaSimplificada extends Omit<CommomInterface, 'id'> {
   despesaSimplificadaId: number;
+  totalValue: number;
   unidadeFederativaId: string;
 }
